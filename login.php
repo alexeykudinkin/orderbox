8b533db4451319df40347b96ad9fb6379d6a3778
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2015-05-06 01:25:51 +0300

    Introduced basic styling

--- login.php
+++ login.php
@@ -13,32 +13,44 @@
 <html>
   <head>
     <title>OrderBox</title>
-    <link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
   </head>
 
   <body>
-    <div id="main">
+
+    <div class="container">
       <h1>OrderBox</h1>
 
-      <div id="login">
+      <div class="col-md-offset-4 col-md-4">
         <h2>Sign in</h2>
 
         <form action="" method="post">
 
-          <label>Email:</label>
-          <input id="email" name="email" placeholder="Email" type="text">
-          <label>Password:</label>
-          <input id="password" name="password" placeholder="Password" type="password">
-          <input name="submit" type="submit" value="Sign in">
+          <div class="form-group">
+            <label for="email">Email:</label>
+            <input id="email" name="email" class="form-control" placeholder="Email" type="text">
+          </div>
 
-          <?php echo count(get_errors()) ?>
+          <div class="form-group">
+            <label for="password">Password:</label>
+            <input id="password" name="password" class="form-control" placeholder="Password" type="password">
+          </div>
+
+          <div class="form-group">
+            <input name="submit" type="submit" value="Sign in" class="btn btn-default">
+          </div>
+
           <?php for ($i = 0; $i < count(get_errors()); ++$i) { ?>
             <span>
               <?php echo get_errors()[$i]; ?>
             </span>
           <?php } ?>
         </form>
+
       </div>
+
     </div>
+
   </body>
+
 </html>
