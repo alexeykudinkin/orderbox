370e21e930072978d558e60c42eef93971b97623
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2015-05-06 01:59:20 +0300

    (VW): Refurbished views to accommodate (common-)header with basic user-bio

--- create.php
+++ create.php
@@ -6,6 +6,16 @@
 
   check_whether_authenticated_or_redirect("login.php");
 
+  include('conn.php');
+
+  $conn = db_conn_open("vk");
+
+  if (!isset($conn)) {
+    push_error("Couldn't connect to the Database!");
+    respond_internal_error();
+    exit;
+  }
+
   include('createX.php'); ?>
 
 <!DOCTYPE html>
@@ -18,52 +28,57 @@
   <body>
 
     <div class="container">
-      <h1>OrderBox</h1>
 
-      <div class="col-md-offset-4 col-md-4">
-        <h2>Create</h2>
+      <?php include('header.php') ?>
 
-        <form class="form-horizontal" action="" method="post">
+      <div class="row">
+        <div class="col-md-offset-4 col-md-4">
+          <h2>Create</h2>
 
-          <hr/>
+          <form class="form-horizontal" action="" method="post">
 
-          <div class="form-group">
-            <label for="short_decription" class="col-sm-2 control-label">Short</label>
-            <div class="col-sm-10">
-              <input id="short_decription" type="text" name="short_description" placeholder="Short description" class="form-control">
+            <hr/>
+
+            <div class="form-group">
+              <label for="short_decription" class="col-sm-2 control-label">Short</label>
+              <div class="col-sm-10">
+                <input id="short_decription" type="text" name="short_description" placeholder="Short description" class="form-control">
+              </div>
             </div>
-          </div>
 
-          <div class="form-group">
-            <label for="full_description" class="col-sm-2 control-label">Full</label>
-            <div class="col-sm-10">
-              <input id="full_description" type="text" name="full_description" placeholder="Full description" class="form-control">
+            <div class="form-group">
+              <label for="full_description" class="col-sm-2 control-label">Full</label>
+              <div class="col-sm-10">
+                <input id="full_description" type="text" name="full_description" placeholder="Full description" class="form-control">
+              </div>
             </div>
-          </div>
 
-          <div class="form-group">
-            <label for="cost" class="col-sm-2 control-label">Cost</label>
-            <div class="col-sm-10">
-              <input id="cost" type="number" name="cost" placeholder="Estimated cost" class="form-control">
+            <div class="form-group">
+              <label for="cost" class="col-sm-2 control-label">Cost</label>
+              <div class="col-sm-10">
+                <input id="cost" type="number" name="cost" placeholder="Estimated cost" class="form-control">
+              </div>
             </div>
-          </div>
 
-          <hr/>
+            <hr/>
 
-          <input name="submit" type="submit" value="Post" class="btn btn-default col-sm-offset-6">
+            <input name="submit" type="submit" value="Post" class="btn btn-default col-sm-offset-6">
 
-        </form>
+          </form>
+
+        </div>
+
+        <?php for ($i = 0; $i < count(get_errors()); ++$i) { ?>
+          <span>
+            <?php echo get_errors()[$i]; ?>
+          </span>
+        <?php } ?>
 
       </div>
-
-      <?php for ($i = 0; $i < count(get_errors()); ++$i) { ?>
-        <span>
-          <?php echo get_errors()[$i]; ?>
-        </span>
-      <?php } ?>
-
     </div>
 
   </body>
 
 </html>
+
+<?php db_conn_close($conn); ?>
--- orders.php
+++ orders.php
@@ -8,6 +8,12 @@
   check_whether_authenticated_or_redirect("login.php");
 
   $conn = db_conn_open("vk");
+
+  if (!isset($conn)) {
+    push_error("Couldn't connect to the Database!");
+    respond_internal_error();
+    exit;
+  }
     
   $q = mysql_query("SELECT orders.*, users.email FROM orders JOIN users ON orders.created_by = users.id", $conn); ?> 
 
@@ -21,63 +27,66 @@
   <body>
 
     <div class="container">
-      <h1>OrderBox</h1>
 
-      <div class="col-md-offset-4 col-md-4">
-        <h2>Orders</h2>
+      <?php include("header.php"); ?>
+      
+      <div class="row">
+        <div class="col-md-offset-4 col-md-4">
+          <h2>Orders</h2>
 
-        <?php 
+          <?php 
 
-          while ($order = mysql_fetch_assoc($q)) { 
+            while ($order = mysql_fetch_assoc($q)) { 
 
-            if (isset($order['executed_by']))
-              continue;
+              if (isset($order['executed_by']))
+                continue;
 
-            $short_description  = htmlspecialchars($order['short_description']);
-            $full_description   = htmlspecialchars($order['full_description']);
-            $customer           = htmlspecialchars($order['email']);
-            $cost               = $order['cost'];
-            $until              = $order['until'];
-            $oid                = $order['id'];     ?>
+              $short_description  = htmlspecialchars($order['short_description']);
+              $full_description   = htmlspecialchars($order['full_description']);
+              $customer           = htmlspecialchars($order['email']);
+              $cost               = $order['cost'];
+              $until              = $order['until'];
+              $oid                = $order['id'];     ?>
 
-            <hr />
-            
-            <div class="panel panel-default">
-
-              <div class="panel-heading">
-                <h4><?php echo $short_description; ?></h4>
-              </div>
-            
-              <div class="panel-body">
-                <p>
-                <?php echo $full_description; ?>
-                </p>
+              <hr />
               
-                <br/>
+              <div class="panel panel-default">
 
-                <span class="label label-info"><?php echo $cost; ?> $</span>
-                <span class="label label-info"><?php echo $until; ?></span>
-                <span class="label label-info"><?php echo $customer; ?></span>
+                <div class="panel-heading">
+                  <h4><?php echo $short_description; ?></h4>
+                </div>
+              
+                <div class="panel-body">
+                  <p>
+                  <?php echo $full_description; ?>
+                  </p>
+                
+                  <br/>
 
-                <!-- CSRF Protection -->
+                  <span class="label label-info"><?php echo $cost; ?> $</span>
+                  <span class="label label-info"><?php echo $until; ?></span>
+                  <span class="label label-info"><?php echo $customer; ?></span>
 
-                <form action="execute.php" method="post" class="pull-right">
-                  <input name="oid" type="hidden" value="<?php echo $oid ?>">
-                  <input name="submit" type="submit" value="Execute" class="btn btn-default">
-                </form>
+                  <!-- CSRF Protection -->
+
+                  <form action="execute.php" method="post" class="pull-right">
+                    <input name="oid" type="hidden" value="<?php echo $oid ?>">
+                    <input name="submit" type="submit" value="Execute" class="btn btn-default">
+                  </form>
+
+                </div>
 
               </div>
+              
+              <hr />
 
-            </div>
-            
-            <hr />
+          <?php 
+            } 
+            db_conn_close($conn); ?>
 
-        <?php 
-          } 
-          db_conn_close($conn); ?>
+          <a class="btn btn-default" href="create.php">Create</a>
 
-        <a class="btn btn-default" href="create.php">Create</a>
-
+        </div>
       </div>
 
     </div>
