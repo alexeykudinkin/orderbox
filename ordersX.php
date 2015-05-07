9e0794f9aa636e5c6b1562fbbd61e32fb031319f
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2015-05-07 21:54:47 +0300

    (CSM): Cosmetics

--- ordersX.php
+++ ordersX.php
@@ -2,59 +2,62 @@
   
   if (isset($_GET['from'])) {
 
-  $from = mysqli_real_escape_string($conn, stripslashes($_GET['from']));
+    $from = mysqli_real_escape_string($conn, stripslashes($_GET['from']));
 
-  $q = mysqli_query($conn, "SELECT orders.*, users.email FROM orders JOIN users ON orders.created_by = users.id WHERE orders.id > $from ORDER BY orders.id DESC"); 
+    $q = mysqli_query($conn, "SELECT orders.*, users.email FROM orders JOIN users ON orders.created_by = users.id WHERE orders.id > $from ORDER BY orders.id DESC"); 
 
-} else {
+  } else {
 
-  $q = mysqli_query($conn, "SELECT orders.*, users.email FROM orders JOIN users ON orders.created_by = users.id ORDER BY orders.id DESC"); 
+    $q = mysqli_query($conn, "SELECT orders.*, users.email FROM orders JOIN users ON orders.created_by = users.id ORDER BY orders.id DESC"); 
 
-}
+  }
 
-while ($order = mysqli_fetch_assoc($q)) { 
+  if (!$q)
+    push_error("Failed to fetch orders!");
 
-  if (isset($order['executed_by']))
-    continue;
+  while ($q && $order = mysqli_fetch_assoc($q)) { 
 
-  $short_description  = htmlspecialchars($order['short_description']);
-  $full_description   = htmlspecialchars($order['full_description']);
-  $customer           = htmlspecialchars($order['email']);
-  $cost               = $order['cost'];
-  $until              = $order['until'];
-  $oid                = $order['id'];     ?>
+    if (isset($order['executed_by']))
+      continue;
 
-  <div class="order">            
+    $short_description  = htmlspecialchars($order['short_description']);
+    $full_description   = htmlspecialchars($order['full_description']);
+    $customer           = htmlspecialchars($order['email']);
+    $cost               = $order['cost'];
+    $until              = $order['until'];
+    $oid                = $order['id'];     ?>
 
-    <div class="panel panel-default">
+    <div class="order">            
 
-      <div class="panel-heading">
-        <h4><?php echo $short_description; ?></h4>
+      <div class="panel panel-default">
+
+        <div class="panel-heading">
+          <h4><?php echo $short_description; ?></h4>
+        </div>
+      
+        <div class="panel-body">
+          <p>
+            <?php echo $full_description; ?>
+          </p>
+        
+          <br/>
+
+          <span class="label label-info"><?php echo $cost; ?> $</span>
+          <span class="label label-info"><?php echo $customer; ?></span>
+
+          <form id="execute" action="execute.php" method="post" class="pull-right">
+            <input name="oid" type="hidden" value="<?php echo $oid ?>">
+            <input name="submit" type="submit" value="Execute" class="btn btn-default">
+          </form>
+
+        </div>
+
       </div>
     
-      <div class="panel-body">
-        <p>
-          <?php echo $full_description; ?>
-        </p>
-      
-        <br/>
-
-        <span class="label label-info"><?php echo $cost; ?> $</span>
-        <span class="label label-info"><?php echo $customer; ?></span>
-
-        <form id="execute" action="execute.php" method="post" class="pull-right">
-          <input name="oid" type="hidden" value="<?php echo $oid ?>">
-          <input name="submit" type="submit" value="Execute" class="btn btn-default">
-        </form>
-
-      </div>
+      <hr/>
 
     </div>
-    
-    <hr/>
-
-  </div>
 
 <?php 
-  } 
-  db_conn_close($conn); ?>
+    } 
+    db_conn_close($conn); ?>
