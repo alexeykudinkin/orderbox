3a1a74f81040532d5b879326806dd84e275a9289
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2015-05-07 11:00:03 +0300

    (INF): Adopted ClearDB

--- connX.php
+++ connX.php
@@ -1,12 +1,20 @@
 <?php 
 
-  function db_conn_open($name) {
-    $conn = mysql_connect("localhost", "root", "");
+  function db_conn_open() {
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server   = $url['host'];
+    $user     = $url['user'];
+    $password = $url['pass'];
+
+    $conn = mysql_connect($server, $username, $pass);
 
     if (!$conn)
       return NULL;
 
-    mysql_select_db($name, $conn);
+    $db = substr($url['path'], 1);
+
+    mysql_select_db($db, $conn);
 
     return $conn;
   }
