e098fb32e5d64d7d97b65b5f2420efe94b804a01
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2015-05-07 21:56:10 +0300

    (INF): Fixed external deps layout in `create.php`

--- create.php
+++ create.php
@@ -19,8 +19,12 @@
 <html>
   <head>
     <title>OrderBox</title>
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-  </head>
+
+      <!-- Unfortunately, it's used inside the header included -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+</head>
 
   <body>
 
