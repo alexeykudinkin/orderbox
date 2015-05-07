ae2950a19841cba1a58c60c27f51f7967a259b95
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2015-05-07 22:21:50 +0300

    (VW): Slightly revisited sign-in page to make error-reports to be more user-friendly

--- login.php
+++ login.php
@@ -13,35 +13,42 @@
 <html>
   <head>
     <title>OrderBox</title>
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+      <!-- Unfortunately, it's used inside the header included -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   </head>
 
   <body>
 
     <div class="container">
-      <h1>OrderBox</h1>
 
-      <div class="col-md-offset-4 col-md-4">
-        <h2>Sign in</h2>
+      <?php include('headerX.php'); ?>
 
-        <form action="" method="post">
+      <div class="row">
+        <div class="col-md-offset-4 col-md-4">
+          <h2>Sign in</h2>
 
-          <div class="form-group">
-            <label for="email">Email:</label>
-            <input id="email" name="email" class="form-control" placeholder="Email" type="text">
-          </div>
+          <form action="" method="post">
 
-          <div class="form-group">
-            <label for="password">Password:</label>
-            <input id="password" name="password" class="form-control" placeholder="Password" type="password">
-          </div>
+            <div class="form-group">
+              <label for="email">Email:</label>
+              <input id="email" name="email" class="form-control" placeholder="Email" type="text">
+            </div>
 
-          <div class="form-group">
-            <input name="submit" type="submit" value="Sign in" class="btn btn-default">
-          </div>
+            <div class="form-group">
+              <label for="password">Password:</label>
+              <input id="password" name="password" class="form-control" placeholder="Password" type="password">
+            </div>
 
-        </form>
+            <div class="form-group">
+              <input name="submit" type="submit" value="Sign in" class="btn btn-default">
+            </div>
 
+          </form>
+
+        </div>
       </div>
 
     </div>
